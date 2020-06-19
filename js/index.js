4e12b123bc5b1e52b452f80cebf2d9b149c5e559
Tristan Guest <tristanguest@gmail.com>
2020-06-19 12:43:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -39,3 +39,15 @@ function bubble(e) {
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }
+
+
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
